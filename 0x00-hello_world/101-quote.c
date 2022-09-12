7e53014033ea9591a4584962a827e12ae46f70a3
abel paul <abelpaul12@gmail.com>
2022-09-12 09:43:21 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-#include<unistd.h>
-
+#include<uninstd.h>
 /**
  * main -Entry point
  * Return : always 1 (success)
  * /
- int main(void)
- {
-write(2,
-" and that piece of art is useful\" -Dora Korparr,2015-10-19\n",59);
-return (1);
-}
+ * int main(void)
+ * {
+ * write(2,
+ * " and that piece of art is useful \" -Dora Korparr, 2015-10-19\n", 59);
+ * return (1);
+ * }
